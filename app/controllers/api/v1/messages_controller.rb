cd19adb93175ca34c7c0bead486d058d23895815
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-09 16:46:11 +0300

    Update created the API routes

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MessagesController < AplicationController 
   def index 
-    @message = Message.all.sample 
+    message = Message.all.sample 
      
-    if @message
+    if message
       render json: {status: "SUCCESS", message: "Message was retrieved succesfully!"}, status: :ok
     else
-      render json: @message.errors, status: :bad_request
+      render json: message.errors, status: :bad_request
     end
   end
 end
