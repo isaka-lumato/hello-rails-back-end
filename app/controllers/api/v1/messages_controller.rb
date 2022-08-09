ce794c9f0acc93225b8cf479683d1bee01e5d36b
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-09 16:51:30 +0300

    Update checked and fixed all found offences

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,9 +1,9 @@
-class Api::V1::MessagesController < AplicationController 
-  def index 
-    message = Message.all.sample 
-     
+class Api::V1::MessagesController < ApplicationController
+  def index
+    message = Message.all.sample
+
     if message
-      render json: {status: "SUCCESS", message: "Message was retrieved succesfully!"}, status: :ok
+      render json: { status: 'SUCCESS', data: message, message: 'Message was retrieved succesfully!' }, status: :ok
     else
       render json: message.errors, status: :bad_request
     end
