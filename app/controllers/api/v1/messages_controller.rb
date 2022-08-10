2a0882e56437ee179f9ba254458c831c65d457e1
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-10 10:49:27 +0300

    Update to my API endpoint due to problems in the front end

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
     message = Message.all.sample
-
-    if message
-      render json: { status: 'SUCCESS', data: message, message: 'Message was retrieved succesfully!' }, status: :ok
-    else
-      render json: message.errors, status: :bad_request
-    end
+    render json: message
   end
 end
